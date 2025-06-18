bacd405ed80b894d11821912f90b648d795f12de
Camila <camilaferna140494@gmail.com>
2025-06-17 21:57:53 -0300

    feat: Define application routes for Consultation and Register components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { ConsultationComponent } from './consultation/consultation.component';
+import { RegisterComponent } from './register/register.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'consultation', component: ConsultationComponent},
+    {path: 'register', component: RegisterComponent},
+
+];
