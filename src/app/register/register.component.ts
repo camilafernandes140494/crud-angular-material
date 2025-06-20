3a9e4ab41b1689d59a619d6f3420b3d5a787e47a
Camila <camilaferna140494@gmail.com>
2025-06-19 21:14:31 -0300

    feat: Implement registration form layout and structure in register component

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,11 +1,21 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+
+import { FlexLayoutModule } from "@angular/flex-layout";
+import { MatCardModule } from "@angular/material/card";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatInputModule } from "@angular/material/input";
+import { FormsModule } from "@angular/forms";
 
 @Component({
-  selector: 'app-register',
-  imports: [],
-  templateUrl: './register.component.html',
-  styleUrl: './register.component.scss'
+  selector: "app-register",
+  imports: [
+    FlexLayoutModule,
+    MatCardModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+  ],
+  templateUrl: "./register.component.html",
+  styleUrl: "./register.component.scss",
 })
-export class RegisterComponent {
-
-}
+export class RegisterComponent {}
