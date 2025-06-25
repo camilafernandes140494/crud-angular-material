616d1e71ad4863de31a79577872ed3340d25f475
Camila <camilaferna140494@gmail.com>
2025-06-24 22:46:44 -0300

    feat: Enhance consultation component with form layout and material design elements

--- src/app/consultation/consultation.component.ts
+++ src/app/consultation/consultation.component.ts
@@ -1,11 +1,22 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { MatIconModule } from "@angular/material/icon";
+import { MatButtonModule } from "@angular/material/button";
+import { MatCardModule } from "@angular/material/card";
+import { MatInputModule } from "@angular/material/input";
+import { FormsModule } from "@angular/forms";
+import { FlexLayoutModule } from "@angular/flex-layout";
 
 @Component({
-  selector: 'app-consultation',
-  imports: [],
-  templateUrl: './consultation.component.html',
-  styleUrl: './consultation.component.scss'
+  selector: "app-consultation",
+  imports: [
+    MatIconModule,
+    MatButtonModule,
+    MatCardModule,
+    MatInputModule,
+    FormsModule,
+    FlexLayoutModule,
+  ],
+  templateUrl: "./consultation.component.html",
+  styleUrl: "./consultation.component.scss",
 })
-export class ConsultationComponent {
-
-}
+export class ConsultationComponent {}
